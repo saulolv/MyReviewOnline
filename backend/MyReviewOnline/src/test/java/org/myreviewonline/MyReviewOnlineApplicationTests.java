33d8d9db5dc9a33d60582cd04ede1bc698a3f31c
saulolv <saulosantos229@gmail.com>
2024-11-16 10:28:55 -0300

    Initial commit

---
+++ backend/MyReviewOnline/src/test/java/org/myreviewonline/MyReviewOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myreviewonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyReviewOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
