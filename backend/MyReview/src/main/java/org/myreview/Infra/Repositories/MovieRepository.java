669f08af41b88bbb81816e0466039f45bf163353
davi muritiba <145371341+davimuritiba@users.noreply.github.com>
2024-11-18 21:57:52 -0300

    Create MovieRepository.java

---
+++ backend/MyReview/src/main/java/org/myreview/Infra/Repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package org.myreview.Domain.Repositories;
+
+import org.myreview.Domain.Entities.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface MovieRepository extends JpaRepository<Movie, UUID> {
+}
