854cd49e43b9627db34be9cb16fdb00d4c257279
davi muritiba <145371341+davimuritiba@users.noreply.github.com>
2024-11-18 22:00:54 -0300

    Create serierepository.java

---
+++ backend/MyReview/src/main/java/org/myreview/Infra/Repositories/serierepository.java
@@ -0,0 +1,9 @@
+package org.myreview.Domain.Repositories;
+
+import org.myreview.Domain.Entities.Series;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface SeriesRepository extends JpaRepository<Series, UUID> {
+}
