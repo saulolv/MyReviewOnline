955f60c05e50613142cc0cc61041258b90bb5283
davi muritiba <145371341+davimuritiba@users.noreply.github.com>
2024-11-18 22:04:18 -0300

    Create episoderepository.java

---
+++ backend/MyReview/src/main/java/org/myreview/Infra/Repositories/episoderepository.java
@@ -0,0 +1,12 @@
+package org.myreview.Domain.Repositories;
+
+import org.myreview.Domain.Entities.Episode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface EpisodeRepository extends JpaRepository<Episode, UUID> {
+    // Buscar episódios por ID da série
+    List<Episode> findBySeriesId(UUID seriesId);
+}
